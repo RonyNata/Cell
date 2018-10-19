2f696ab3933e03dece51d7f67458dc896747cd6b
Unknown <rony.natak@gmail.com>
2018-10-19 18:28:58 +0700

    final

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -6,6 +6,6 @@
     .module('cell')
     .constant('malarkey', malarkey)
     .constant('moment', moment)
-    .constant('API', 'http://10.2.1.32:8989/');
+    .constant('API', 'http://10.2.1.57:80/');
 
 })();
